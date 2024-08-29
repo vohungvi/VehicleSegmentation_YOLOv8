3cf8e077a68664611ce4ad38fed46248d031df7d
DESKTOP-G6UFDDQ\i5 <khanhtoanrealler@gmail.com>
2024-08-29 10:49:38 +0700

    + add count vehicles

---
+++ count.py
@@ -0,0 +1,44 @@
+import cv2
+from ultralytics import YOLO, solutions
+
+def count_Vehicles(model=None, count_mode=None):
+    model = YOLO(model)
+    cap = cv2.VideoCapture("highway.mp4")
+    assert cap.isOpened(), "Error reading video file"
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Define line points
+    if count_mode == 'line':
+        points = [(0, 400), (width, 400)]
+    elif count_mode == 'region':
+        points = [(760, 100), (width-700, 100), (width, 600), (0, 600)]
+
+    video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
+    counter = solutions.ObjectCounter(
+        view_img=True,
+        reg_pts=points,
+        names=model.names,
+        draw_tracks=True,
+        line_thickness=2,
+    )
+
+    while cap.isOpened():
+        success, im0 = cap.read()
+        if not success:
+            print("Video frame is empty or video processing has been successfully completed.")
+            break
+        tracks = model.track(im0, persist=True, show=False, device=0)
+
+        im0 = counter.start_counting(im0, tracks)
+        video_writer.write(im0)
+
+    cap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+
+if __name__=="__main__":
+    count_Vehicles(model='./runs/detect/train/weights/best.pt', count_mode='region')
+
